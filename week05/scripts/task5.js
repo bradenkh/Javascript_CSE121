6c33adfd48a8b088f10e85e4bc2b013a44c716dd
bradenkh <bradenkh@byui.edu>
2021-10-12 12:08:18 -0600

    added 05 functions

--- week05/scripts/task5.js
+++ week05/scripts/task5.js
@@ -3,35 +3,65 @@
 /* IF/ELSE IF */
 
 // Step 1: Declare and initialize a new variable to hold the current date
+var today = new Date();
 
 // Step 2: Declare another variable to hold the day of the week
-
 // Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
+var dayOfWeek = today.getDay();
 
 // Step 4: Declare a variable to hold a message that will be displayed
-
+var msg = "";
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
-
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
-
+if (dayOfWeek > 0 && dayOfWeek < 6) {
+  msg = "Hang in there!";
+} else {
+  msg = "Woohoo!  It is the weekend!";
+}
 
 /* SWITCH, CASE, BREAK */
-
 // Step 1: Declare a new variable to hold another message
-
+var msg2 = "";
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
-
+switch (dayOfWeek) {
+  case 0:
+    msg2 = "Sunday";
+    break;
+  case 1:
+    msg2 = "Monday";
+    break;
+  case 2:
+    msg2 = "Tuesday";
+    break;
+  case 3:
+    msg2 = "Wednesday";
+    break;
+  case 4:
+    msg2 = "Thursday";
+    break;
+  case 5:
+    msg2 = "Friday";
+    break;
+  case 6:
+    msg2 = "Saturday";
+    break;
+  default:
+    throw "bad day of week";
+    break;
+}
 
 /* OUTPUT */
 
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
+document.getElementById("message1").innerText = msg;
 
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
-
+document.getElementById("message2").innerText = msg2;
 
 /* FETCH */
 
 // Step 1: Declare a global empty array variable to store a list of temples
+var temples = [];
 
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
 // - Creates an HTML <article> element
@@ -41,16 +71,39 @@
 // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of temples
+function output(myArray) {
+    myArray.forEach((element) => {
+        console.log("hello");
+        var thisTemple = document.createElement("article");
+        var templeName = document.createElement("h3");
+        templeName.innerText = element.templeName;
+        var location = document.createElement("h4").innerText = element.location;
+        var dedicated = document.createElement("h4").innerText = element.dedicated;
+        var img = document.createElement("h3").setAttribute("src") = element.imageUrl;
+        thisTemple.appendChild(templeName);
+        thisTemple.append(location);
+        thisTemple.append(dedicated);
+        thisTemple.append(img);
+        document.getElementById("temples").appendChild(thisTemple);
+    });
+    return;
+}
 
 // Step 3: Using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'
-
 // Step 4: Add a .then() method to turn the returned string into a JavaScript object ( hint: .json() )
-
 // Step 5: Add another .then() method with a variable name to hold the temples and an empty arrow function
-
 // Step 6: Inside of second .then() method, assign the list of temples (as a JSON object) to the temples variable
+fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            temples.push(element);
+        });
+    });
 
 // Step 7: Finally, call the output function and pass it the list of temples
+console.log(typeof(temples));
+output(temples);
 
 // Step 8: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
 
@@ -60,7 +113,6 @@
 // - Calls the output function passing in the sorted list of temples
 
 // Step 10: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
-
 
 /* STRETCH */
 
