8b7808c436e74850ebc79ed6e700ab6a635ef6f4
bradenkh <bradenkh@byui.edu>
2021-10-14 12:59:30 -0600

    added asc and desc sort functionality

--- week05/scripts/task5.js
+++ week05/scripts/task5.js
@@ -14,9 +14,9 @@ var msg = "";
 // Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
 // Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
 if (dayOfWeek > 0 && dayOfWeek < 6) {
-  msg = "Hang in there!";
+   msg = "Hang in there!";
 } else {
-  msg = "Woohoo!  It is the weekend!";
+   msg = "Woohoo!  It is the weekend!";
 }
 
 /* SWITCH, CASE, BREAK */
@@ -24,30 +24,30 @@ if (dayOfWeek > 0 && dayOfWeek < 6) {
 var msg2 = "";
 // Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
 switch (dayOfWeek) {
-  case 0:
-    msg2 = "Sunday";
-    break;
-  case 1:
-    msg2 = "Monday";
-    break;
-  case 2:
-    msg2 = "Tuesday";
-    break;
-  case 3:
-    msg2 = "Wednesday";
-    break;
-  case 4:
-    msg2 = "Thursday";
-    break;
-  case 5:
-    msg2 = "Friday";
-    break;
-  case 6:
-    msg2 = "Saturday";
-    break;
-  default:
-    throw "bad day of week";
-    break;
+   case 0:
+      msg2 = "Sunday";
+      break;
+   case 1:
+      msg2 = "Monday";
+      break;
+   case 2:
+      msg2 = "Tuesday";
+      break;
+   case 3:
+      msg2 = "Wednesday";
+      break;
+   case 4:
+      msg2 = "Thursday";
+      break;
+   case 5:
+      msg2 = "Friday";
+      break;
+   case 6:
+      msg2 = "Saturday";
+      break;
+   default:
+      throw "bad day of week";
+      break;
 }
 
 /* OUTPUT */
@@ -62,6 +62,7 @@ document.getElementById("message2").innerText = msg2;
 
 // Step 1: Declare a global empty array variable to store a list of temples
 var temples = [];
+var templesAscend;
 
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
 // - Creates an HTML <article> element
@@ -72,24 +73,25 @@ var temples = [];
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of temples
 function output(myArray) {
-    console.log("running output");
-    myArray.forEach((element) => {
-        var thisTemple = document.createElement("article");
-        var templeName = document.createElement("h3");
-        templeName.innerText = element.templeName;
-        var location = document.createElement("h4");
-        location.innerText = element.location;
-        var dedicated = document.createElement("h4");
-        dedicated.innerText = element.dedicated;
-        var img = document.createElement("img");
-        img.setAttribute("src", element.imageUrl);
-        thisTemple.appendChild(templeName);
-        thisTemple.append(location);
-        thisTemple.append(dedicated);
-        thisTemple.append(img);
-        document.getElementById("temples").appendChild(thisTemple);
-    });
-    return;
+   console.log("running output");
+   document.getElementById("temples").innerHTML = "";
+   myArray.forEach((element) => {
+      var thisTemple = document.createElement("article");
+      var templeName = document.createElement("h3");
+      templeName.innerText = element.templeName;
+      var location = document.createElement("h4");
+      location.innerText = element.location;
+      var dedicated = document.createElement("h4");
+      dedicated.innerText = element.dedicated;
+      var img = document.createElement("img");
+      img.setAttribute("src", element.imageUrl);
+      thisTemple.appendChild(templeName);
+      thisTemple.append(location);
+      thisTemple.append(dedicated);
+      thisTemple.append(img);
+      document.getElementById("temples").appendChild(thisTemple);
+   });
+   return;
 }
 
 // Step 3: Using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'
@@ -98,31 +100,52 @@ function output(myArray) {
 // Step 6: Inside of second .then() method, assign the list of temples (as a JSON object) to the temples variable
 // Step 7: Finally, call the output function and pass it the list of temples
 fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log("running fetch");
-        data.forEach(element => {
-            temples.push(element);
-        });
-        output(temples);
-    });
-
-
+   .then((response) => response.json())
+   .then((data) => {
+      console.log("running fetch");
+      data.forEach((element) => {
+         temples.push(element);
+      });
+      output(temples);
+      temples.sort()
+      templesAscend = true;
+      sortBy(temples);
+   });
 
 // Step 8: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
 function reset() {
-  var articles = document.querySelectorAll("#temples");
-  articles.forEach(item => {item.innerHTML = "";});
+   var articles = document.querySelectorAll("#temples");
+   articles.forEach((item) => {
+      item.innerHTML = "";
+   });
+   document.getElementById("temples").innerHTML = "<h3>Loading temples...</h3>";
 }
 
 // Step 9: Declare a function named sortBy that does the following:
 // - Calls the reset function
 // - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
 // - Calls the output function passing in the sorted list of temples
-
+function sortBy(temples) {
+   if (document.getElementById("sortBy").value == "templeNameDescending") {
+      if (templesAscend) {
+         temples.reverse();
+         templesAscend = false;        
+      }
+      output(temples); 
+   }
+   else {
+      if (!templesAscend) {
+         temples.reverse();
+         templesAscend = true;
+      }
+      output(temples);
+   }
+}
 
 // Step 10: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
-
+document.getElementById("sortBy").addEventListener("change", () => {
+   sortBy(temples, document.getElementById("sortBy").value);
+});
 /* STRETCH */
 
 // Consider adding a "Filter by" feature that allows users to filter the list of temples
